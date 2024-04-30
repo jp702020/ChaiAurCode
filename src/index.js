4ee77db2f30733e2e7aa33fc90666aef3e3d8427
Patel Junaid <jp70202021@gmail.com>
2024-05-01 00:36:55 +0530

    adding express function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 // require("dotenv").config({ path: "./env" });
 
-import connectDB from "./db/index.js";
+import connectDB from "./db/index.js ";
 import dotenv from "dotenv";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+const port = process.env.PORT || 8000;
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERROR : ", error);
+      throw error;
+    });
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGOODB CONECCTION FAILED FROM src/index.js", err);
+  });
