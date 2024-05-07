62ea52c072ffe2522510384f0f3abd3238d2d209
Patel Junaid <jp70202021@gmail.com>
2024-05-08 00:20:56 +0530

    fix some bug in login and logout

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -90,7 +90,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
 const loginUser = asyncHandler(async (req, res) => {
   const { email, username, password } = req.body;
-  if (!(username || email)) {
+  if (!username && !email) {
     throw new ApiError(400, "Username or email is required");
   }
 
@@ -112,8 +112,7 @@ const loginUser = asyncHandler(async (req, res) => {
   );
 
   const loggedInUser = await User.findById(user._id).select(
-    "-password",
-    "-refreshToken"
+    "-password -refreshToken"
   );
 
   const options = {
--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -1,6 +1,6 @@
-import { User } from "../models/user.models";
-import { ApiError } from "../utils/ApiError";
-import { asyncHandler } from "../utils/asyncHandler";
+import { User } from "../models/user.models.js";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
@@ -14,11 +14,9 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
     }
 
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-
-    const user = await User.findById(decodedToken?._id).select(
+    const user = await User.findById(decodedToken?.id).select(
       "-password -refreshToken"
     );
-
     if (!user) {
       throw new ApiError(401, "Invalid Access Token");
     }
